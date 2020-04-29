55714fdc48a09dc45197b10817967ab0869a9b9b
Paul Kompfner <kompfner@gmail.com>
2020-04-29 10:48:14 -0400

    Improve dev scripts: `npm i` first packs `daily-js` and `react-native-js`, and `npm sync-daily` is simpler

---
+++ DailyPlayground/scripts/pack-daily.sh
@@ -0,0 +1,16 @@
+DAILY_JS_DIR=~/src/pluot-core/daily-js
+RN_DAILY_JS_DIR=~/src/rn-daily-js-playground/react-native-daily-js
+
+# Metro bundler can't follow symlinks so we can't rely on npm's normal support
+# for local packages. That's why we need to consume @daily-co/daily-js and
+# @daily-co/react-native-daily-js as packed files.
+
+# `npm pack` @daily-co/daily-js
+pushd $DAILY_JS_DIR
+npm pack
+popd
+
+# `npm pack` @daily-co/react-native-daily-js
+pushd $RN_DAILY_JS_DIR
+npm pack
+popd
