49b49c0b9e8cc18d9aa3b46a61bfbe8a1ece766b
Paul Kompfner <kompfner@gmail.com>
2020-05-05 10:18:27 -0400

    Add WebRTC APIs to global namespace when loading `react-native-daily-js`

--- react-native-daily-js/src/index.ts
+++ react-native-daily-js/src/index.ts
@@ -1,5 +1,11 @@
 import DailyIframe = require("@daily-co/daily-js");
+import { registerGlobals } from "react-native-webrtc";
+
+// Set up globals:
+// * URL
+// * WebRTC APIs
 import 'react-native-url-polyfill/auto';
+registerGlobals();
 
 export default DailyIframe;
 export * from "react-native-webrtc";
