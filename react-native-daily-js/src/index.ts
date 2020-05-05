0552eda91c69d83ee4bf7e3358b380120efa7e5e
Paul Kompfner <kompfner@gmail.com>
2020-05-05 16:19:30 -0400

    Note (in a comment) that adding WebRTC APIs to global namespace also adds the global `window` object.

--- react-native-daily-js/src/index.ts
+++ react-native-daily-js/src/index.ts
@@ -3,7 +3,7 @@ import { registerGlobals } from "react-native-webrtc";
 
 // Set up globals:
 // * URL
-// * WebRTC APIs
+// * WebRTC APIs (note that this also sets up the global `window` object)
 import 'react-native-url-polyfill/auto';
 registerGlobals();
 
