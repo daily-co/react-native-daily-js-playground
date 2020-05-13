5428469bcec49810fdaa2f0969dcaebc60190263
Paul Kompfner <kompfner@gmail.com>
2020-05-13 19:38:01 -0400

    Add some global shims to prevent errors in `pluot-core`

--- react-native-daily-js/src/index.ts
+++ react-native-daily-js/src/index.ts
@@ -1,11 +1,15 @@
 import DailyIframe = require("@daily-co/daily-js");
 import { registerGlobals } from "react-native-webrtc";
 
-// Set up globals:
-// * URL
+// Register globals:
+// * A URL polyfill
+import "react-native-url-polyfill/auto";
 // * WebRTC APIs (note that this also sets up the global `window` object)
-import 'react-native-url-polyfill/auto';
 registerGlobals();
+// * (Temporary) A couple shims to prevent errors (not ideal)
+declare const global: any;
+global.window.location = { href: ''};
+global.window.addEventListener = () => {};
 
 export default DailyIframe;
 export * from "react-native-webrtc";
