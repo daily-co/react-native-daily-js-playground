9aec8a9a3f7977c97ea7dcc86ce3a142599e813e
Paul Kompfner <kompfner@gmail.com>
2020-07-07 18:55:57 -0400

    Update `npm run install-dev` and add `npm run sync-webrtc` scripts to enable local iteration on `react-native-webrtc` package much like `@daily-co/react-native-daily-js` and `@daily-co/daily-js`

---
+++ DailyPlayground/scripts/pack-webrtc.sh
@@ -0,0 +1,9 @@
+# Note that the current script is meant to be run as an `npm` command under 
+# DailyPlayground, hence the path relative to that directory
+. ./scripts/variables.sh
+
+# `npm pack` react-native-webrtc
+pushd $RN_WEBRTC_DIR
+rm react-native-webrtc-*.tgz
+npm pack
+popd
