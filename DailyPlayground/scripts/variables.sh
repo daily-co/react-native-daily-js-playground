b59e93fdbf7cba303cbcec37bfe8675eaf658d87
Paul Kompfner <kompfner@gmail.com>
2020-07-07 18:55:57 -0400

    Update `npm run install-dev` and add `npm run sync-webrtc` scripts to enable local iteration on `react-native-webrtc` package much like `@daily-co/react-native-daily-js` and `@daily-co/daily-js`

--- DailyPlayground/scripts/variables.sh
+++ DailyPlayground/scripts/variables.sh
@@ -1,3 +1,4 @@
 PLAYGROUND_DIR=~/src/rn-daily-js-playground/DailyPlayground
 RN_DAILY_JS_DIR=~/src/pluot-core/react-native-daily-js
 DAILY_JS_DIR=~/src/pluot-core/daily-js
+RN_WEBRTC_DIR=~/src/react-native-webrtc
