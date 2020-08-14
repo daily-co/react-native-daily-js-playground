b5fdb0abb1a8a707a0f32f20dfd8f1a2716f6ff7
Paul Kompfner <kompfner@gmail.com>
2020-08-14 10:22:22 -0400

    Correct path for symlinking local Android react-native-webrtc code when running `npm run install-dev`

--- DailyPlayground/scripts/symlink-android-code.sh
+++ DailyPlayground/scripts/symlink-android-code.sh
@@ -3,7 +3,7 @@
 . ./scripts/variables.sh
 
 RN_DAILY_JS_NODE_MODULES_DIR=./node_modules/@daily-co/react-native-daily-js
-RN_WEBRTC_NODE_MODULES_DIR=./node_modules/react-native-webrtc
+RN_WEBRTC_NODE_MODULES_DIR=./node_modules/@daily-co/react-native-webrtc
 
 rm -rf $RN_DAILY_JS_NODE_MODULES_DIR/android && ln -s $RN_DAILY_JS_DIR/android $RN_DAILY_JS_NODE_MODULES_DIR
 rm -rf $RN_WEBRTC_NODE_MODULES_DIR/android && ln -s $RN_WEBRTC_DIR/android $RN_WEBRTC_NODE_MODULES_DIR
