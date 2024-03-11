221968ac3e6c0c88c618c7efa82c2507c7dedf5e
Filipi Fuchter <filipi87@gmail.com>
2024-03-11 15:32:41 -0300

    Updating to RN 0 73.6

--- DailyPlayground/metro.config.js
+++ DailyPlayground/metro.config.js
@@ -1,41 +1,11 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
 /**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
  *
- * @format
+ * @type {import('metro-config').MetroConfig}
  */
+const config = {};
 
-function isUrl(value) {
-  try {
-    // eslint-disable-next-line no-new
-    new URL(value);
-    return true;
-  } catch {
-    return false;
-  }
-}
-
-module.exports = {
-  symbolicator: {
-    customizeFrame: (frame) => {
-      if (frame.file && isUrl(frame.file)) {
-        return {
-          ...frame,
-          // HACK: This prevents Metro from attempting to read the invalid file URL it sent us.
-          lineNumber: null,
-          column: null,
-          // This prevents the invalid frame from being shown by default.
-          collapse: true,
-        };
-      }
-    },
-  },
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
