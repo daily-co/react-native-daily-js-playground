ae58da03ffc82561b3ff393827cab44cfff05568
mattie ruth backman <mattieruth@gmail.com>
2025-04-18 16:53:43 -0400

    Upgraded react-native to 0.76.9 and Updated project to use expo
    Todo: This works except that it is missing the unit tests and
    the ScreenCapture extension for ios.

--- DailyPlayground/index.js
+++ DailyPlayground/index.js
@@ -1,21 +1,8 @@
-/**
- * @format
- */
+import {registerRootComponent} from 'expo';
 
-// Enable debug logs
-/*window.localStorage = window.localStorage || {};
-window.localStorage.debug = '*';
-window.localStorage.getItem = (itemName) => {
-  console.log('Requesting the localStorage item ', itemName);
-  return window.localStorage[itemName];
-};*/
-
-//Disable webrtc logs
-/*import debug from 'debug';
-debug.disable('rn-webrtc:*');*/
-
-import {AppRegistry} from 'react-native';
 import App from './src/components/App/App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
