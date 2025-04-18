a89f902b2dff04faf096aeaa7d4eca55f3ffa82f
mattie ruth backman <mattieruth@gmail.com>
2025-04-18 16:53:43 -0400

    Upgraded react-native to 0.76.9 and Updated project to use expo
    Todo: This works except that it is missing the unit tests and
    the ScreenCapture extension for ios.

--- DailyPlayground/metro.config.js
+++ DailyPlayground/metro.config.js
@@ -1,11 +1,7 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = config;
