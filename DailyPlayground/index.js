4738774c3a09c37f2c034497adea056f4051f5fa
Filipi Fuchter <filipi87@gmail.com>
2025-05-16 18:06:46 -0300

    Restoring the options to enable the logs.

--- DailyPlayground/index.js
+++ DailyPlayground/index.js
@@ -1,5 +1,17 @@
 import {registerRootComponent} from 'expo';
 
+// Enable debug logs
+/*window.localStorage = window.localStorage || {};
+window.localStorage.debug = '*';
+window.localStorage.getItem = (itemName) => {
+  console.log('Requesting the localStorage item ', itemName);
+  return window.localStorage[itemName];
+};*/
+
+//Disable webrtc logs
+/*import debug from 'debug';
+debug.disable('rn-webrtc:*');*/
+
 import App from './src/components/App/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
